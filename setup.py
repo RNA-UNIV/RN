b428ba9dba1ac7d947d1486db5a915ea574d5e19
César <cesar.es@gmail.com>
2024-07-21 01:36:46 -0300

    Actualizacion de dataset de dedos

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="rna",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     package_data={
         "": ["descargas/**/*"],
     },
     install_requires=[
-        # Dependencias del paquete
+        "requests",
+        "pandas",
+        "chardet",
+        "json",
+        "numpy",
+        "zipfile"
     ],
     author="César",
     author_email="tu@email.com",
